b0e76f85ed5b3a040d73cf3d29707d1b0f76dac5
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-08-13 16:12:18 +0900

    Add: gem 'devise'を導入し、ユーザー登録をできるように機能を追加

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module StampApp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dirroot.[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
-  end
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get "signup", to: "users/registrations#new"
+    get "login", to: "users/sessions#new"
+    get "logout", to: "users/sessions#destroy"
+  end
 end
