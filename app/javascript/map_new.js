da205a8bd198d470e25f2301d4c8d24deca39254
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-13 13:18:31 +0900

    mapを編集できるように指定

--- app/javascript/map_new.js
+++ app/javascript/map_new.js
@@ -1,21 +1,23 @@
 document.addEventListener('turbo:load', function() {
   const mapContainers = document.querySelectorAll('[data-controller="map-container"]');
   mapContainers.forEach((container) => {
-    initMap(container);
+    const lat = parseFloat(container.querySelector('.lat').value) || 43.9965; // 網走監獄
+    const lng = parseFloat(container.querySelector('.lng').value) || 144.2305;
+    initMap(container, lat, lng);
   });
 });
 
-function initMap(containerElement) {
+function initMap(containerElement, initialLat = 43.9965, initialLng = 144.2305) {
   const geocoder = new google.maps.Geocoder();
 
   const mapElement = containerElement.querySelector('.map');
   const map = new google.maps.Map(mapElement, {
-    center:  {lat: 35.6803997, lng:139.7690174},  //東京
+    center: {lat: initialLat, lng: initialLng},
     zoom: 15,
   });
 
   const marker = new google.maps.Marker({
-    position: {lat: 40.7828, lng: -73.9653},
+    position: {lat: initialLat, lng: initialLng},
     map: map,
     draggable: true
   });
@@ -23,31 +25,31 @@ function initMap(containerElement) {
   containerElement.mapInstance = map;
   containerElement.geocoderInstance = geocoder;
   containerElement.markerInstance = marker;
+
+  // マーカーのドロップ（ドラッグ終了）時のイベント
+  google.maps.event.addListener(marker, 'dragend', function(ev){
+    containerElement.querySelector('.lat').value = ev.latLng.lat();
+    containerElement.querySelector('.lng').value = ev.latLng.lng();
+  });
 }
 
 function codeAddress(containerElement) {
   const geocoder = containerElement.geocoderInstance;
   const map = containerElement.mapInstance;
   const marker = containerElement.markerInstance;
-  
+
   let inputAddress = containerElement.querySelector('.address').value;
-  
+
   geocoder.geocode({ 'address': inputAddress }, function(results, status) {
     if (status == 'OK') {
       map.setCenter(results[0].geometry.location);
       marker.setPosition(results[0].geometry.location);
-      
+
       containerElement.querySelector('.lat').value = results[0].geometry.location.lat();
       containerElement.querySelector('.lng').value = results[0].geometry.location.lng();
     } else {
       alert('該当する結果がありませんでした：' + status);
     }
-  });
-
-  // マーカーのドロップ（ドラッグ終了）時のイベント
-  google.maps.event.addListener(marker, 'dragend', function(ev){
-    containerElement.querySelector('.lat').value = ev.latLng.lat();
-    containerElement.querySelector('.lng').value = ev.latLng.lng();
   });
 }
 
