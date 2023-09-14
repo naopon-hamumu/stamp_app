2f6c6f9404de12f2c7383c31e3efdb5f0762734f
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-14 13:37:32 +0900

    map_show

--- app/javascript/map_show.js
+++ app/javascript/map_show.js
@@ -1,23 +1,44 @@
-export const MapShow = {
-  init: function(elementId, lat, lng) {
-      let map;
-      let position = { lat: lat, lng: lng };
-
-      // 要素IDを基にマップ要素を取得
-      let mapElement = document.getElementById(elementId);
-
-      if (mapElement) {
-          map = new google.maps.Map(mapElement, {
-              center: position,
-              zoom: 15
-          });
-
-          let marker = new google.maps.Marker({
-              position: position,
-              map: map
-          });
-      } else {
-          console.error("Element with ID:", elementId, "not found.");
+const MapShow = {
+    init: function(elementId, lat, lng) {
+        let map;
+        let position = { lat: lat, lng: lng };
+  
+        // 要素IDを基にマップ要素を取得
+        let mapElement = document.getElementById(elementId);
+  
+        if (mapElement) {
+            map = new google.maps.Map(mapElement, {
+                center: position,
+                zoom: 15
+            });
+  
+            let marker = new google.maps.Marker({
+                position: position,
+                map: map
+            });
+        } else {
+            console.error("Element with ID:", elementId, "not found.");
+        }
+    }
+  }
+  
+  function initializeMaps() {
+      if (document.querySelector('.stamp-rally-show-page')) {
+        document.querySelectorAll('[id^="map-show-"]').forEach((mapElement, index) => {
+          const stamp = window.stamps[index];
+          if (stamp && mapElement) {
+            const lat = parseFloat(stamp.latitude);
+            const lng = parseFloat(stamp.longitude);
+            
+            MapShow.init(mapElement.id, lat, lng);
+          }
+        });
       }
   }
-}
+  
+  // Turboの読み込みイベントでマップ初期化
+  document.addEventListener('turbo:load', initializeMaps);
+  
+  // 通常の読み込みイベントでもマップ初期化
+  document.addEventListener('DOMContentLoaded', initializeMaps);
+  
