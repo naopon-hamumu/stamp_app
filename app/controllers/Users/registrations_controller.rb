d41cb0ba3d8fac834f15ad0130a071b8c5e42391
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-17 06:20:56 +0900

    ユーザーの編集の変更

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -4,6 +4,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
