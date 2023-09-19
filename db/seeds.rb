53a82dc9bbc4f21c2ad19e60b2edb07f9c3f2012
Nao <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-19 19:19:53 +0900

    Revert "Page nation"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-10.times do
-  user = User.create!(
-    name: Faker::Name.unique.name,
-    email: Faker::Internet.email,
-    password: 'password',
-    password_confirmation: 'password'
-  )
-
-  2.times do
-    user.stamp_rallies.create!(
-      title: 'そうちゃんかわゆい',
-      description: Faker::Quote.famous_last_words,
-      visibility: :public_open,
-      stamps_attributes: [
-        {
-          name: 'スタンプ',
-          sticker: File.open("./app/assets/images/ham1.jpg"),
-          latitude: Faker::Address.latitude,
-          longitude: Faker::Address.longitude,
-          address: Faker::Address.full_address
-        }
-      ]
-    )
-  end
-end
