22bcaecc05ffddc04356fb02e3d04a1f1d815f4f
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-19 10:31:05 +0900

    time

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module StampApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
