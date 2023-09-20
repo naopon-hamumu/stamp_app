4c9a6b42323f8b26074e9b4674d4ed247ebf50a1
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-20 21:18:53 +0900

    remove activerecord strage

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,6 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
+
+  validates :uid, presence: true, uniqueness: true
+  validates :provider, presence: true, uniqueness: { scope: :user_id }
 end
