e907ea1e1675150a7ac122ac28ba88b3e720a4c5
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-09-21 20:51:07 +0900

    ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,13 +30,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('omg.jpg'),
+        image: image_url('ogp.jpg'),
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary_large_image',
         site: '@',
-        image: image_url('omg.jpg')
+        image: image_url('ogp.jpg')
       }
     }
   end
