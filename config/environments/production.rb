b8402c7c9a1c01d3ebbd148d1d74c8afff1bde0e
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-10-18 17:25:49 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'stamp-bon-voyage@hamuhamu.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'stamp-bon-voyage.com',
+    user_name:            ENV['GMAIL'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
